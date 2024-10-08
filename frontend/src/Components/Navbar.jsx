56519591c4979f2b343f31295faa3af9968ce457
Jaloqa <mykola.rabotnikov@gmail.com>
2024-10-08 23:36:17 +0200

    Frontend creating products, ColorTheme, Toast for errors

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,13 @@
-import {Button, Container, Flex, HStack, Text } from "@chakra-ui/react";
+import {Button, Container, Flex, HStack, Text, useColorMode } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { CiSquarePlus } from "react-icons/ci";
+import {CiSquarePlus } from "react-icons/ci";
+import { FiSun, FiMoon } from "react-icons/fi";
+
 const Navbar = () => {
+
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Container maxW={"1140px"} px={4}>
+    <Container maxW={"1140px"} px={4} >
       <Flex
         h={16}
         alignItems={"center"}
@@ -27,8 +31,12 @@ const Navbar = () => {
           <Link to="/create">
           <Button>
             <CiSquarePlus fontSize={20} />  
+            
           </Button>
           </Link>
+          <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? <FiMoon /> : <FiSun />}
+          </Button>
 
         </HStack>
       </Flex>
